eae26133f854ad43bdfd7e9f59954c903c46960d
sullis <github@seansullivan.com>
2024-07-28 12:44:23 -0700

    check for allocation failures in MutableOffHeapByteArrayStoreTest

--- pinot-segment-local/src/test/java/org/apache/pinot/segment/local/io/writer/impl/MutableOffHeapByteArrayStoreTest.java
+++ pinot-segment-local/src/test/java/org/apache/pinot/segment/local/io/writer/impl/MutableOffHeapByteArrayStoreTest.java
@@ -19,10 +19,13 @@
 package org.apache.pinot.segment.local.io.writer.impl;
 
 import java.util.Arrays;
+import org.apache.pinot.segment.spi.memory.PinotDataBuffer;
 import org.apache.pinot.segment.spi.memory.PinotDataBufferMemoryManager;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
@@ -40,6 +43,16 @@ public class MutableOffHeapByteArrayStoreTest {
   public void tearDown()
       throws Exception {
     _memoryManager.close();
+  }
+
+  @BeforeMethod
+  public void beforeMethodAssertions() {
+    Assert.assertEquals(PinotDataBuffer.getAllocationFailureCount(), 0);
+  }
+
+  @AfterMethod
+  public void afterMethodAssertions() {
+    Assert.assertEquals(PinotDataBuffer.getAllocationFailureCount(), 0);
   }
 
   @Test
